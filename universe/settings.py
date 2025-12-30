e240c9d6b898d945390d8f472175a00881ea97a4
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2025-12-30 15:29:32 +0600

    Update the authentication UI with icons, placeholder, and user friendly background

--- universe/settings.py
+++ universe/settings.py
@@ -149,6 +149,18 @@ STATICFILES_DIRS = [
     # You can add more directories here as needed
 ]
 
+# email configuration
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
